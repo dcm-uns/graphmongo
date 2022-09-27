34d779b437c4b3b12c056a69c59b8100ddff1151
dcm@uns <38795567+dcm-uns@users.noreply.github.com>
2022-09-27 09:45:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,16 +22,22 @@ var app = Express();
 var cors = require("cors");
 app.use(cors());
 
+// Usamos mongoose como conector a la Base de Datos "amigos"
 mongoose
 	.connect("mongodb://127.0.0.1:27017/amigos")
 	.then(() => console.log("Connected to database..."))
 	.catch(err => console.error(err));
 
+// Definimos el modelo de la base de datos, en este caso "Person", 
+// formada solo por dos strings: firstName y lastName.
 const PersonModel = mongoose.model("person", {
 	firstName: String,
 	lastName: String
 });
 
+// El anterior es para interactuar con mongoDB de manera simple
+// Lo siguiente es para ofrecer al mundo el tipo Person como dato GrahQL
+// Describe el mismo tipo de dato, pero con los tipos GraphQL
 const PersonType = new GraphQLObjectType({
 	name: "Person",
 	fields: {
@@ -41,35 +47,35 @@ const PersonType = new GraphQLObjectType({
 	}
 });
 
+// Aqui la definición completa del esquema de nuestro endpoint.
+// Tenemos consultas y mutaciones.
 const schema = new GraphQLSchema({
 	query: new GraphQLObjectType({
 		name: "Query",
 		fields: {
-			// Query 1
-
-			// name of the query, people
+			// Consulta "people"
 			people: {
-				 // the type of response this query will return, here PersonType
 				type: GraphQLList(PersonType),
-				// resolver is required
+				// Lo siguiente es la implementacion de esta consulta
+				// que usa el modelo para pedirle datos a Mongo.
 				resolve: (root, args, context, info) => {
-					// we are returning all persons available in the table in mongodb
 					return PersonModel.find().exec();
 				}
 			},
-			// Query 2
+			// consulta peopleByID
 			peopleByID: {
-				// name of the query is people by id
 				type: PersonType,
+				// requiere un argumenti ID, que no podrá ser nulo.
 				args: {
-					// strong validation for graphqlid, which is mendatory for running this query
 					id: { type: GraphQLNonNull(GraphQLID) }
 				},
+				// Lo siguiente es la implementación de la consulta
+				// que al igual que la anterior, usa PersonModel para comunicarnos con Mongo
 				resolve: (root, args, context, info) => {
 					return PersonModel.findById(args.id).exec();
 				}
 			},
-			// Query 3
+			// Otra consulta similar a las anteriores.
 			peopleByName: {
 				type: GraphQLList(PersonType),
 				args: { 
@@ -82,8 +88,9 @@ const schema = new GraphQLSchema({
 		}
 	}),
 
-	// Mutation 1
+	// Las mutacipones son las acciones de modificación de datos en nuestro endpoint.
 	mutation: new GraphQLObjectType({
+		// Nombre de la mutación y los datos que recibe.
 		name: "Create",
 		fields: {
 			people: {
@@ -92,6 +99,8 @@ const schema = new GraphQLSchema({
 					firstName: { type: GraphQLString },
 					lastName: { type: GraphQLString }
 				},
+				// La implementación de la mutación nuevamente usa mongoose.
+				// Simplemente guarda el dato que nos pasan.
 				resolve: (root, args, context, info) => {
 					var people = new PersonModel(args);
 					return people.save();
@@ -101,7 +110,10 @@ const schema = new GraphQLSchema({
 	})
 });
 
-app.use("/amigos",	ExpressGraphQL({schema, graphiql: true}));
+// Cuando en nuestra applicacion la URL tiene "/amigos", quien procesa esto es ExpressGraphQL.
+// Este es el motor de ejecución que hará lo que dijimos en las líneas anteriores.
+// Además deseamos que GraphiQL esté disponible.
+app.use("/amigos", ExpressGraphQL({schema, graphiql: true}));
 
 app.listen(3001, () => {
 	console.log("server running at 3001");
